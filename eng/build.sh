bc25285d111ad597e13eba71da62288be94efbf5
Jan Jones <janjones@microsoft.com>
2024-07-23 16:44:35 +0200

    Add build script

---
+++ eng/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
+chmod +x dotnet-install.sh
+./dotnet-install.sh --jsonfile global.json --install-dir ./dotnet
+./dotnet/dotnet --version
+./dotnet/dotnet publish -o output src/RazorInternals
