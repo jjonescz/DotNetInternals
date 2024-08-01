c0b05d839e428ea5f4ee643f6d20b06073d21abf
Jan Joneš <jjones@outlook.cz>
2024-08-01 20:09:22 +0200

    Test backwards compatibility

--- test/DotNetInternals.Tests/CompressorTests.cs
+++ test/DotNetInternals.Tests/CompressorTests.cs
@@ -18,4 +18,33 @@ public class CompressorTests
         var uncompressed = Compressor.Uncompress(compressed);
         Assert.Equal(savedState.Inputs.Single(), uncompressed.Inputs.Single());
     }
+
+    [Fact]
+    public void BackwardsCompatibility()
+    {
+        // Do not change this string, we need to ensure it's always successfully parsed
+        // to ensure old URLs can be opened in new versions of the app.
+        var state = """
+            48rhEg5JLS5xzs8tyM9LzSvRK0qsyi8SCrVBEVUISCxKzLVVMlRS0Lfj4nJIzk9JVajmUgCCaLBUaklqUaxCQWlSTmayQiZMg0K1QnpqibVCMYio5aoFAA
+            """;
+        var actual = Compressor.Uncompress(state);
+        var expected = new SavedState()
+        {
+            Inputs =
+            [
+                new()
+                {
+                    FileName = "TestComponent.razor",
+                    Text = """
+                        <TestComponent Param="1" />
+
+                        @code {
+                            [Parameter] public int Param { get; set; }
+                        }
+                        """.ReplaceLineEndings("\n"),
+                }
+            ]
+        };
+        Assert.Equal(expected.Inputs.Single(), actual.Inputs.Single());
+    }
 }
