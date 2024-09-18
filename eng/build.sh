d540b98d45449950955496e706d6e7528f6477f8
Jan Jones <janjones@microsoft.com>
2024-09-18 11:04:39 +0200

    Install workloads

--- eng/build.sh
+++ eng/build.sh
@@ -3,5 +3,6 @@ curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh --jsonfile global.json --install-dir ./dotnet
 ./dotnet/dotnet --version
+./dotnet/dotnet workload install wasm-tools wasm-experimental
 ./dotnet/dotnet publish -o output src/App
 rm output/wwwroot/_framework/*.wasm
