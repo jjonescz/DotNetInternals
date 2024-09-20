639d1322f29ed792d49beff5ccefabfe2f47a521
Jan Jones <janjones@microsoft.com>
2024-09-20 11:28:08 +0200

    Improve completion item mapping

--- src/Worker/Utils/MonacoConversions.cs
+++ src/Worker/Utils/MonacoConversions.cs
@@ -29,12 +29,58 @@ public static class MonacoConversions
         return new MonacoCompletionItem
         {
             LabelAsString = completion.DisplayText,
-            Kind = completion.Tags.Contains(WellKnownTags.Method) ? CompletionItemKind.Method : CompletionItemKind.Variable,
+            Kind = getKind(completion.Tags),
             RangeAsObject = lines.GetLinePositionSpan(completion.Span).ToRange(),
-            InsertText = completion.DisplayText,
+            InsertText = completion.TryGetInsertionText(out var insertionText) ? insertionText : completion.DisplayText,
             FilterText = completion.FilterText,
             SortText = completion.SortText,
         };
+
+        static CompletionItemKind getKind(ImmutableArray<string> tags)
+        {
+            foreach (var tag in tags)
+            {
+                switch (tag)
+                {
+                    case WellKnownTags.Public: return CompletionItemKind.Keyword;
+                    case WellKnownTags.Protected: return CompletionItemKind.Keyword;
+                    case WellKnownTags.Private: return CompletionItemKind.Keyword;
+                    case WellKnownTags.Internal: return CompletionItemKind.Keyword;
+                    case WellKnownTags.File: return CompletionItemKind.File;
+                    case WellKnownTags.Project: return CompletionItemKind.File;
+                    case WellKnownTags.Folder: return CompletionItemKind.Folder;
+                    case WellKnownTags.Assembly: return CompletionItemKind.File;
+                    case WellKnownTags.Class: return CompletionItemKind.Class;
+                    case WellKnownTags.Constant: return CompletionItemKind.Constant;
+                    case WellKnownTags.Delegate: return CompletionItemKind.Function;
+                    case WellKnownTags.Enum: return CompletionItemKind.Enum;
+                    case WellKnownTags.EnumMember: return CompletionItemKind.EnumMember;
+                    case WellKnownTags.Event: return CompletionItemKind.Event;
+                    case WellKnownTags.ExtensionMethod: return CompletionItemKind.Method;
+                    case WellKnownTags.Field: return CompletionItemKind.Field;
+                    case WellKnownTags.Interface: return CompletionItemKind.Interface;
+                    case WellKnownTags.Intrinsic: return CompletionItemKind.Text;
+                    case WellKnownTags.Keyword: return CompletionItemKind.Keyword;
+                    case WellKnownTags.Label: return CompletionItemKind.Text;
+                    case WellKnownTags.Local: return CompletionItemKind.Variable;
+                    case WellKnownTags.Namespace: return CompletionItemKind.Module;
+                    case WellKnownTags.Method: return CompletionItemKind.Method;
+                    case WellKnownTags.Module: return CompletionItemKind.Module;
+                    case WellKnownTags.Operator: return CompletionItemKind.Operator;
+                    case WellKnownTags.Parameter: return CompletionItemKind.Value;
+                    case WellKnownTags.Property: return CompletionItemKind.Property;
+                    case WellKnownTags.RangeVariable: return CompletionItemKind.Variable;
+                    case WellKnownTags.Reference: return CompletionItemKind.Reference;
+                    case WellKnownTags.Structure: return CompletionItemKind.Struct;
+                    case WellKnownTags.TypeParameter: return CompletionItemKind.TypeParameter;
+                    case WellKnownTags.Snippet: return CompletionItemKind.Snippet;
+                    case WellKnownTags.Error: return CompletionItemKind.Text;
+                    case WellKnownTags.Warning: return CompletionItemKind.Text;
+                }
+            }
+
+            return CompletionItemKind.Text;
+        }
     }
 
     public static LinePosition ToLinePosition(this Position position)
@@ -79,5 +125,12 @@ public static class MonacoConversions
             EndLineNumber = span.End.Line + 1,
             EndColumn = span.End.Character + 1,
         };
+    }
+
+    public static bool TryGetInsertionText(
+        this RoslynCompletionItem completionItem,
+        [NotNullWhen(returnValue: true)] out string? insertionText)
+    {
+        return completionItem.Properties.TryGetValue("InsertionText", out insertionText);
     }
 }
