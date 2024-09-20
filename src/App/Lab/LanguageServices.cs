3f5c49a3fc1fcdbdbdfe5e50d31f35ba2e6b9aa8
Jan Jones <janjones@microsoft.com>
2024-09-20 12:56:09 +0200

    Add debouncing

--- src/App/Lab/LanguageServices.cs
+++ src/App/Lab/LanguageServices.cs
@@ -8,6 +8,41 @@ internal sealed class LanguageServices(IJSRuntime jsRuntime, WorkerController wo
 {
     private string? currentModelUrl;
     private Task changingModel = Task.CompletedTask;
+    private CancellationTokenSource completionCts = new();
+    private CancellationTokenSource diagnosticsCts = new();
+
+    private static Task<TOut> DebounceAsync<TIn, TOut>(ref CancellationTokenSource cts, TIn args, TOut fallback, Func<TIn, Task<TOut>> handler)
+    {
+        cts.Cancel();
+        cts = new();
+
+        return debounceAsync(cts.Token, args, fallback, handler);
+
+        static async Task<TOut> debounceAsync(CancellationToken token, TIn args, TOut fallback, Func<TIn, Task<TOut>> handler)
+        {
+            try
+            {
+                await Task.Delay(TimeSpan.FromSeconds(1), token);
+
+                token.ThrowIfCancellationRequested();
+
+                return await handler(args);
+            }
+            catch (OperationCanceledException)
+            {
+                return fallback;
+            }
+        }
+    }
+
+    private static void Debounce<T>(ref CancellationTokenSource cts, T args, Func<T, Task> handler)
+    {
+        DebounceAsync(ref cts, args, 0, async args =>
+        {
+            await handler(args);
+            return 0;
+        });
+    }
 
     public async Task RegisterAsync()
     {
@@ -15,10 +50,19 @@ internal sealed class LanguageServices(IJSRuntime jsRuntime, WorkerController wo
         await CompletionItemProviderAsync.Register(jsRuntime, cSharpLanguageSelector, new()
         {
             TriggerCharacters = [" ", "(", "=", "#", ".", "<", "[", "{", "\"", "/", ":", ">", "~"],
-            ProvideCompletionItemsFunc = async (modelUri, position, context) =>
+            ProvideCompletionItemsFunc = (modelUri, position, context) =>
             {
-                await changingModel;
-                return await worker.ProvideCompletionItemsAsync(modelUri, position, context);
+                return DebounceAsync(
+                    ref completionCts,
+                    (modelUri, position, context),
+                    new() { Suggestions = [], Incomplete = true },
+                    async args =>
+                    {
+                        // Wait until the content change is sent to the worker.
+                        await changingModel;
+
+                        return await worker.ProvideCompletionItemsAsync(args.modelUri, args.position, args.context);
+                    });
             },
             ResolveCompletionItemFunc = (completionItem) => Task.FromResult(completionItem),
         });
@@ -44,15 +88,19 @@ internal sealed class LanguageServices(IJSRuntime jsRuntime, WorkerController wo
         OnTextUpdated();
     }
 
-    private async void OnTextUpdated()
+    private void OnTextUpdated()
     {
         if (currentModelUrl == null)
         {
             return;
         }
 
-        var markers = await worker.GetDiagnosticsAsync();
-        var model = await BlazorMonaco.Editor.Global.GetModel(jsRuntime, currentModelUrl);
-        await BlazorMonaco.Editor.Global.SetModelMarkers(jsRuntime, model, "LanguageServices", markers.ToList());
+        Debounce(ref diagnosticsCts, (worker, jsRuntime, currentModelUrl), static async args =>
+        {
+            var (worker, jsRuntime, currentModelUrl) = args;
+            var markers = await worker.GetDiagnosticsAsync();
+            var model = await BlazorMonaco.Editor.Global.GetModel(jsRuntime, currentModelUrl);
+            await BlazorMonaco.Editor.Global.SetModelMarkers(jsRuntime, model, "LanguageServices", markers.ToList());
+        });
     }
 }
