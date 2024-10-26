30bb766955747c98404a013574b194c100633d75
Jan Jones <janjones@microsoft.com>
2024-10-26 16:26:11 +0200

    Find DLLs via rehydrate.cmd

--- src/Worker/Lab/AzDoDownloader.cs
+++ src/Worker/Lab/AzDoDownloader.cs
@@ -31,7 +31,36 @@ internal sealed class AzDoDownloader
             buildId: build.Id,
             artifactName: $"Transport_Artifacts_Windows_{buildConfiguration}");
 
-        throw new InvalidOperationException($"Found artifact {artifact.Name}.");
+        var files = await GetArtifactFilesAsync(
+            buildId: build.Id,
+            artifact: artifact);
+
+        var rehydrates = files.Items.Where(f => f.Path.EndsWith("/rehydrate.cmd", StringComparison.Ordinal));
+
+        var fileName = "Microsoft.CodeAnalysis.dll";
+
+        foreach (var rehydrate in rehydrates)
+        {
+            if (rehydrate.Blob is null)
+            {
+                continue;
+            }
+
+            var rehydrateContent = await GetFileAsStringAsync(
+                buildId: build.Id,
+                artifactName: artifact.Name,
+                fileId: rehydrate.Blob.Id);
+
+            foreach (var match in AzDoPatterns.RehydrateCommand.Matches(rehydrateContent).Cast<Match>())
+            {
+                if (match.Groups[1].ValueSpan.Equals(fileName, StringComparison.Ordinal))
+                {
+                    throw new InvalidOperationException($"Found file '{fileName}' in '{match.Groups[2].ValueSpan}'.");
+                }
+            }
+        }
+
+        throw new InvalidOperationException($"No file '{fileName}' in artifact '{artifact.Name}' of build {build.Id}.");
     }
 
     private async Task<Build> GetLatestPrBuildAsync(int definitionId, int pullRequestNumber)
@@ -71,12 +100,56 @@ internal sealed class AzDoDownloader
         return await client.GetFromJsonAsync<BuildArtifact>(uri.ToString(), options)
             ?? throw new InvalidOperationException($"No artifact '{artifactName}' found in build {buildId}.");
     }
+
+    private async Task<ArtifactFiles> GetArtifactFilesAsync(int buildId, BuildArtifact artifact)
+    {
+        return await GetFilesAsync(
+            buildId: buildId,
+            artifactName: artifact.Name,
+            fileId: artifact.Resource.Data);
+    }
+
+    private async Task<ArtifactFiles> GetFilesAsync(int buildId, string artifactName, string fileId)
+    {
+        var uri = GetFileUri(
+            buildId: buildId,
+            artifactName: artifactName,
+            fileId: fileId);
+
+        return await client.GetFromJsonAsync<ArtifactFiles>(uri, options)
+            ?? throw new InvalidOperationException($"No files found in artifact '{artifactName}' of build {buildId}.");
+    }
+
+    private async Task<string> GetFileAsStringAsync(int buildId, string artifactName, string fileId)
+    {
+        return await client.GetStringAsync(GetFileUri(
+            buildId: buildId,
+            artifactName: artifactName,
+            fileId: fileId));
+    }
+
+    private static string GetFileUri(int buildId, string artifactName, string fileId)
+    {
+        var uri = new UriBuilder(baseAddress);
+        uri.AppendPathSegments("_apis", "build", "builds", buildId.ToString(), "artifacts");
+        uri.AppendQuery("artifactName", artifactName);
+        uri.AppendQuery("fileId", fileId);
+        uri.AppendQuery("fileName", string.Empty);
+        uri.AppendQuery("api-version", "7.1");
+        return uri.ToString();
+    }
 }
 
 internal enum BuildConfiguration
 {
     Debug,
     Release,
+}
+
+internal static partial class AzDoPatterns
+{
+    [GeneratedRegex("""^mklink /h %~dp0\\(.*) %HELIX_CORRELATION_PAYLOAD%\\(.*) > nul\r?$""", RegexOptions.Multiline)]
+    public static partial Regex RehydrateCommand { get; }
 }
 
 internal sealed class AzDoCollection<T>
@@ -131,4 +204,21 @@ internal sealed class TypeConverterJsonConverter<T>(TypeConverter typeConverter)
             writer.WriteNullValue();
         }
     }
+}
+
+internal sealed class ArtifactFiles
+{
+    public required ImmutableArray<ArtifactFile> Items { get; init; }
+}
+
+internal sealed class ArtifactFile
+{
+    public required string Path { get; init; }
+    public ArtifactFileBlob? Blob { get; init; }
+}
+
+internal sealed class ArtifactFileBlob
+{
+    public required string Id { get; init; }
+    public required long Size { get; init; }
 }
