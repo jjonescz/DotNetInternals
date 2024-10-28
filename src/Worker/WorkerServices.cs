1afd46f678674cc4f77d7d4674e11baf44582417
Jan Jones <janjones@microsoft.com>
2024-10-28 17:40:53 +0100

    Fix tests

--- src/Worker/WorkerServices.cs
+++ src/Worker/WorkerServices.cs
@@ -6,7 +6,12 @@ namespace DotNetInternals;
 
 public static class WorkerServices
 {
-    public static IServiceProvider Create(string baseUrl, bool debugLogs)
+    public static IServiceProvider CreateTest(HttpMessageHandler? httpMessageHandler = null)
+    {
+        return Create(baseUrl: "http://localhost", debugLogs: true, httpMessageHandler);
+    }
+
+    public static IServiceProvider Create(string baseUrl, bool debugLogs, HttpMessageHandler? httpMessageHandler = null)
     {
         var services = new ServiceCollection();
         services.AddLogging(builder =>
@@ -18,7 +23,7 @@ public static class WorkerServices
 
             builder.AddProvider(new SimpleConsoleLoggerProvider());
         });
-        services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseUrl) });
+        services.AddScoped(sp => new HttpClient(httpMessageHandler ?? new HttpClientHandler()) { BaseAddress = new Uri(baseUrl) });
         services.AddScoped<CompilerLoaderServices>();
         services.AddScoped<AssemblyDownloader>();
         services.AddScoped<CompilerProxy>();
