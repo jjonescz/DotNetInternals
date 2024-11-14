def1dee47311b07a1236c836f615b14586cb4ecc
Jan Jones <janjones@microsoft.com>
2024-11-14 11:09:03 +0100

    Add "syntax + trivia" view

--- src/RoslynAccess/RoslynAccessors.cs
+++ src/RoslynAccess/RoslynAccessors.cs
@@ -1,4 +1,4 @@
-using Microsoft.CodeAnalysis;
+﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Test.Utilities;
 
@@ -9,6 +9,57 @@ public static class RoslynAccessors
     public static string Dump(this CSharpSyntaxNode node)
     {
         return node.Dump();
+    }
+
+    public static string DumpExtended(this CSharpSyntaxNode node)
+    {
+        return TreeDumper.DumpCompact(nodeOrTokenToTree(node));
+
+        static TreeDumperNode nodeOrTokenToTree(SyntaxNodeOrToken nodeOrToken)
+        {
+            string text = nodeOrToken.Kind().ToString();
+
+            if (nodeOrToken.AsNode(out var node))
+            {
+                return new TreeDumperNode(text, null,
+                    [
+                        ..node.GetLeadingTrivia().Select(triviaToTree),
+                        ..node.ChildNodesAndTokens().Select(nodeOrTokenToTree),
+                        ..node.GetTrailingTrivia().Select(triviaToTree),
+                    ]);
+            }
+
+            return new TreeDumperNode(text + " " + stringOrMissing(nodeOrToken), null,
+                [
+                    ..nodeOrToken.GetLeadingTrivia().Select(triviaToTree),
+                    ..nodeOrToken.GetTrailingTrivia().Select(triviaToTree),
+                ]);
+        }
+
+        static TreeDumperNode triviaToTree(SyntaxTrivia trivia)
+        {
+            return new TreeDumperNode($"""
+                {trivia.Kind()} "{withoutNewLines(trivia.ToString())}"
+                """, null,
+                trivia.GetStructure() is { } structure ? [nodeOrTokenToTree(structure)] : []);
+        }
+
+        static string stringOrMissing(SyntaxNodeOrToken nodeOrToken)
+        {
+            if (!nodeOrToken.IsMissing)
+            {
+                return $"""
+                    "{withoutNewLines(nodeOrToken.ToString())}"
+                    """;
+            }
+
+            return "<missing>";
+        }
+
+        static string withoutNewLines(string s)
+        {
+            return s.ReplaceLineEndings("⏎");
+        }
     }
 
     public static string GetDiagnosticsText(this IEnumerable<Diagnostic> actual)
