c6cb0a51aa8d1c0cbd5e9c7d0edafe112ea3e6f1
Jan Jones <janjones@microsoft.com>
2024-11-14 11:58:05 +0100

    Wait for initialization before compiling

--- src/App/Logging.cs
+++ src/App/Logging.cs
@@ -3,4 +3,10 @@ namespace DotNetInternals;
 internal static class Logging
 {
     public static LogLevel LogLevel { get; set; } = LogLevel.Information;
+
+    public static void LogErrorAndAssert(this ILogger logger, string message)
+    {
+        logger.LogError(message);
+        Debug.Fail(message);
+    }
 }
