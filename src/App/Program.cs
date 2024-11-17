d5e105f8e0bc1a1b1cf7a4a0465123ed76bd4d83
Jan Jones <janjones@microsoft.com>
2024-11-17 17:39:41 +0100

    Replace BS with Fluent

--- src/App/Program.cs
+++ src/App/Program.cs
@@ -3,6 +3,7 @@ using DotNetInternals;
 using DotNetInternals.Lab;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.FluentUI.AspNetCore.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,6 +11,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddFluentUIComponents();
 
 builder.Services.AddScoped<WorkerController>();
 builder.Services.AddScoped<LanguageServices>();
