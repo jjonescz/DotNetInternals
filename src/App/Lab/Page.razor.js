5b35f53bd0e1841731c07f4869b4ccf2ddf3ac25
Jan Jones <janjones@microsoft.com>
2024-12-04 14:26:59 +0100

    Compile on Ctrl+S (#45)

---
+++ src/App/Lab/Page.razor.js
@@ -0,0 +1,8 @@
+﻿export function registerEventListeners(dotNetObj) {
+    document.addEventListener('keydown', (e) => {
+        if (e.ctrlKey && e.key === 's') {
+            e.preventDefault();
+            dotNetObj.invokeMethodAsync('CompileAndRenderAsync');
+        }
+    });
+}
