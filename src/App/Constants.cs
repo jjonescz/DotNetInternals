241430117bd09d729129a244e65af7924836a0b8
Jan Jones <janjones@microsoft.com>
2025-02-08 19:53:56 +0100

    Fix custom color

---
+++ src/App/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace DotNetInternals;
+
+/// <remarks>
+/// Keep in sync with <c>:root</c> in <c>app.css</c>.
+/// </remarks>
+static class AppColors
+{
+    public static readonly string CustomDark = "#8967AA";
+}
